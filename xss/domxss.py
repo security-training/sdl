07ce15ff80e808ddcd8eb37d0b674d94b724a6ec
tal <sadasd@g.com>
2021-01-05 10:26:34 +0200

    fix dom xss

--- xss/domxss.py
+++ xss/domxss.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 import urllib.parse
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def d():
-    return "<h1>Enter a number in the URL, for example: http://localhost:5000/5"
-
 @app.route("/<q>")
-def calc(q):
+def d(q):
+    if q == None:
+        return "<h1>Enter a number in the URL query string, for example: http://localhost:5000/5"
+    else:
+        response = make_response(redirect("/?q={}".format(q)))
+        return response
+
+@app.route("/")
+def calc():
     html = """
     <style>td {{text-align: left;}} .green {{text-shadow: 2px 2px 5px green;}}</style>
     <h1 class='green'>Magic Numbers</h1>
-    <div id='area'></div>
+    <div id='area'>Enter a number in the URL query string, for example: http://localhost:5000/5</div>
     <img src='static/xss.jpg'>
     <script>
-    q='{}';
+    q=document.location.search.split('?')[1].split('=')[1];
     if (!Number.isNaN(parseInt(q)) && typeof(parseInt(q))=='number' && q.match(/[^0-9]/g)==null)
     {{
         document.getElementById('area').innerHTML=unescape(q)+' is a number';
@@ -27,5 +31,5 @@ def calc(q):
     }}
     </script>
     """
-    return html.format(q)
+    return html.format()
     #return "<body><script>document.body.innerHTML=eval({})</script></body>".format(urllib.parse.unquote(request.args.get('e')))
